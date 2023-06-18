edc30fc94a7833a16c747211165542654a9c64ab
Luca v. K <luca.v.kannen@gmail.com>
2023-06-18 14:58:45 +0200

    Now loading thanos mesh but texture and animation not working

--- src/objects/Flower/Flower.js
+++ src/objects/Flower/Flower.js
@@ -1,17 +1,51 @@
-import { Group } from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import MODEL from './Flower.glb';
+import { Group, TextureLoader, AnimationMixer, Clock } from 'three';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
+import MODEL from './Thanos.fbx';
+import TEXTURE from './ThanosTextur.png';
 
 export default class Flower extends Group {
   constructor() {
-    const loader = new GLTFLoader();
-    
     super();
 
     this.name = 'flower';
 
-    loader.load(MODEL, (gltf)=>{
-      this.add(gltf.scene);
+    const fbxLoader = new FBXLoader();
+    const textureLoader = new TextureLoader();
+    this.mixer = null; // Animation mixer
+    this.clock = new Clock(); // Clock for managing animation time
+
+    // Load texture
+    const texture = textureLoader.load(TEXTURE);
+
+    // Load model
+    fbxLoader.load(MODEL, (model) => {
+      model.scale.set(0.01, 0.01, 0.01); // Adjust scale as per your need
+      model.traverse((child) => {
+        if (child.isMesh) {
+          // Apply texture to the model
+          child.material.map = texture;
+        }
+      });
+
+      // Create an animation mixer and apply it to the model
+      this.mixer = new AnimationMixer(model);
+
+      // If there are animations in the model, play them
+      if(model.animations.length > 0){
+        model.animations.forEach((clip) => {
+          this.mixer.clipAction(clip).play();
+        });
+      }
+
+      // Add model to the group
+      this.add(model);
     });
+  }
+
+  update(deltaTime) {
+    // If there's an animation mixer, update it with the time delta
+    if(this.mixer) {
+      this.mixer.update(this.clock.getDelta());
+    }
   }
 }
